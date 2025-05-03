19ed7715ab8cb08cdd06ead3bcc81f3bc367dec3
Sophia Vo <sovo@ucsd.edu>
2025-05-02 21:45:07 -0700

    lab5*

--- projects/projects.js
+++ projects/projects.js
@@ -10,7 +10,7 @@ let query = '';
 let selectedIndex = -1;
 
 function updateList(data) {
-  countEl.textContent = `(${data.length})`;
+  countEl.textContent = `${data.length}`;
   renderProjects(data, projectsContainer, 'h2');
 }
 
@@ -36,43 +36,36 @@ function renderPie(data) {
   const arcs = pieGen(pieData);
   const colorScale = d3.scaleOrdinal(d3.schemeTableau10);
 
-
-  
   svg.selectAll('path')
-  .data(arcs)
-  .join('path')
-    .attr('d', d3.arc().innerRadius(0).outerRadius(50))
-    .attr('fill', (_, i) => colorScale(i))
-    .style('cursor', 'pointer')
-    .on('click', (event, d) => {
-      
-  d3.selectAll('#projects-pie-plot path').classed('selected', false);
-  d3.selectAll('.legend li').classed('selected', false);
+    .data(arcs)
+    .join('path')
+      .attr('d', arcGen)
+      .attr('fill', (_, i) => colorScale(i))
+      .style('cursor', 'pointer')
+      .on('click', (event, d) => {
+        d3.selectAll('#projects-pie-plot path').classed('selected', false);
+        d3.selectAll('.legend li').classed('selected', false);
 
-  d3.select(event.currentTarget).classed('selected', true);
+        d3.select(event.currentTarget).classed('selected', true);
+        d3.select(`.legend li:nth-child(${d.index + 1})`)
+          .classed('selected', true);
 
-  d3.select(`.legend li:nth-child(${d.index + 1})`)
-    .classed('selected', true);
-    
-      selectedIndex = (selectedIndex === d.index ? -1 : d.index);
-      applySelection();
-      filterAndUpdate();
-    });
-    
-    
+        selectedIndex = (selectedIndex === d.index ? -1 : d.index);
+        applySelection();
+        filterAndUpdate();
+      });
 
-
-      legend.selectAll('li')
-      .data(arcs)
-      .join('li')
-        .attr('style', (d, i) => `--color:${colorScale(i)}`)
-        .html(d => `<span class="swatch"></span>${d.data.label} <em>(${d.data.value})</em>`)
-        .style('cursor', 'pointer')
-        .on('click', (event, d) => {
-          selectedIndex = (selectedIndex === d.index ? -1 : d.index);
-          applySelection();
-          filterAndUpdate();
-        });
+  legend.selectAll('li')
+    .data(arcs)
+    .join('li')
+      .attr('style', (d, i) => `--color: ${colorScale(i)}`)
+      .html(d => `<span class="swatch"></span>${d.data.label} <em>(${d.data.value})</em>`)
+      .style('cursor', 'pointer')
+      .on('click', (event, d) => {
+        selectedIndex = (selectedIndex === d.index ? -1 : d.index);
+        applySelection();
+        filterAndUpdate();
+      });
 
   applySelection();
 }
@@ -81,53 +74,26 @@ function applySelection() {
   d3.selectAll('#projects-pie-plot path')
     .attr('opacity', (_, i) => selectedIndex === -1 || selectedIndex === i ? 1 : 0.5)
     .classed('selected', (_, i) => selectedIndex === i);
+
   d3.selectAll('.legend li')
     .attr('opacity', (_, i) => selectedIndex === -1 || selectedIndex === i ? 1 : 0.5)
     .classed('selected', (_, i) => selectedIndex === i);
 }
 
-// function filterAndUpdate() {
-//   const searchData = projects.filter(p =>
-//     Object.values(p)
-//       .join(' ')
-//       .toLowerCase()
-//       .includes(query.toLowerCase())
-//   );
-
-//   let listData = searchData;
-//   if (selectedIndex !== -1) {
-//     const pieData = getPieData(searchData);
-//     const year    = pieData[selectedIndex].label;
-//     listData = searchData.filter(p => p.year === year);
-//   }
-
-//   const noEl   = document.querySelector('.no-results');
-//   const chartC = document.querySelector('.chart-container');
-//   if (listData.length === 0) {
-//     noEl.hidden = false;
-//     chartC.classList.add('hidden');
-//   } else {
-//     noEl.hidden = true;
-//     chartC.classList.remove('hidden');
-//   }
-
-//   updateList(listData);
-//   renderPie(searchData);
-// }
-
 function filterAndUpdate() {
-  const yearFiltered = (selectedIndex !== -1)
-    ? projects.filter(p => p.year === getPieData(projects)[selectedIndex].label)
-    : projects;
-
-  const searchData = yearFiltered.filter(p =>
+  const searchData = projects.filter(p =>
     Object.values(p)
       .join(' ')
       .toLowerCase()
       .includes(query.toLowerCase())
   );
 
-  const listData = searchData;
+  let listData = searchData;
+  if (selectedIndex !== -1) {
+    const pieData = getPieData(searchData);
+    const year    = pieData[selectedIndex].label;
+    listData = searchData.filter(p => p.year === year);
+  }
 
   const noEl   = document.querySelector('.no-results');
   const chartC = document.querySelector('.chart-container');
@@ -143,12 +109,12 @@ function filterAndUpdate() {
   renderPie(searchData);
 }
 
-
-
+// wire up search bar
 searchInput.addEventListener('input', e => {
   query = e.target.value;
   filterAndUpdate();
 });
 
+// initial render
 updateList(projects);
 renderPie(projects);
